23d31e99f647e6403f80deb476995864ae71b267
bjorn-ali-goransson <bjorn.a.goransson@gmail.com>
2020-12-04 20:40:08 +0100

    Debug iterate through blocks

--- src/test_app.cpp
+++ src/test_app.cpp
@@ -11,6 +11,16 @@ int main(int argc, char **argv) {
 
 	printf("File blender version: %s\n", data.hdr()->version().c_str()); // => get hdr
 
+	printf("Number of blocks: %i\n", (int)data.blocks()->size());
+
+	for(auto &block : *data.blocks()){
+		printf("Types: \n");
+
+		for(auto &type : *block->body()->types()){
+			printf("  %s\n", type.c_str());
+		}
+	}
+
 	return 0;
 }
 

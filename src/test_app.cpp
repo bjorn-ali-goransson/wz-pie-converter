1326811fbb08ba63893ffee274b8c814e95dd813
bjorn-ali-goransson <bjorn.a.goransson@gmail.com>
2020-12-05 13:22:51 +0100

    Refactor to datapart

--- src/test_app.cpp
+++ src/test_app.cpp
@@ -3,7 +3,22 @@
 #include <kaitai/kaitaistream.h>
 #include "blender_blend.h"
 
+class DataPart {
+	public:
+	DataPart(blender_blend_t &data, blender_blend_t::file_block_t &block, blender_blend_t::dna_struct_t &type){
+		printf("%s (%s)\n", block.code().c_str(), type.type().c_str());
+		
+		for(auto &_field : *type.fields()){
+			printf("  %s (%s)\n", _field->name().c_str(), _field->type().c_str());
+		}
+	}
+};
+
 class DataBlock {
+	private:
+		std::unique_ptr<DataPart> part;
+
+
 	public:
 	DataBlock(blender_blend_t &data, std::string code){
 		for(auto &block : *data.blocks()){
@@ -16,12 +31,12 @@ class DataBlock {
 
 			auto &type = *data.sdna_structs()->at(block->sdna_index());
 
-			printf("%s (%s)\n", block->code().c_str(), type.type().c_str());
-			
-			for(auto &_field : *type.fields()){
-				printf("%s (%s)\n", _field->name().c_str(), _field->type().c_str());
-			}
+			part = std::unique_ptr<DataPart>(new DataPart(data, *block, type));
 		}
+	}
+
+	void getPart(std::string name){
+		// std::unique_ptr<kaitai::kstream>(new kaitai::kstream(m__raw_body))
 	}
 };
 
@@ -34,6 +49,8 @@ int main(int argc, char **argv) {
 	printf("File blender version: %s\n", data.hdr()->version().c_str()); // => get hdr
 
 	std::unique_ptr<DataBlock> mesh = std::unique_ptr<DataBlock>(new DataBlock(data, "ME"));
+
+	mesh->getPart("ID");
 
 	// this is for listing data types
 	// for(auto &sdna_struct : *data.sdna_structs()){
